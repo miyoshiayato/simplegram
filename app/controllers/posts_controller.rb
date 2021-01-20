5b8fa6cb7405632848199a7aa045be1f424656c5
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-01-20 22:23:07 +0900

    mergeメソッドにてuser_idの情報を統合

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:caption,phots_attributes: [:image])
+      params.require(:post).permit(:caption,phots_attributes: [:image]).merge(user_id: current_user.id)
     end
 end
