9c321f742a53b020f75877d0e96be6777fc90679
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-01-20 22:15:43 +0900

    投稿が保存されても、されなくてもroot_pathにリダイレクトするように記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,12 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.photos.present?
-      
+      @post.save
+      redirect_to root_path
+      flash[:notice] = "投稿が保存されました"
+    else
+      redirect_to root_path
+      flash[:alert] = "投稿に失敗しました"
+    end
+  end
 end
