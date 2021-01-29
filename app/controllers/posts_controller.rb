91e208140cb9cbb3d13786a3b6dcc136b54e2f30
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-01-29 18:29:38 +0900

    条件式追記

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,13 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find_by(id: params[:id])
     if @post.user == current_user
-      
+      if @post.destroy
+        flash[:notice] = "投稿が削除されました"
+      end
+    else
+      flash[:alert] = "投稿の削除に失敗しました"
+    end
+    redirect_to root_path
   end
 
   private
