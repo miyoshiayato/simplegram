eddc5fbcba8bc48b73e6732d33994ccd990ae132
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-02-03 20:29:54 +0900

    commentsコントローラーにcreateアクションを作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @post = @comment.post
+    if @comment.save
+      respond_to :js
+    else
+      flash[:alert] = "コメントに失敗しました"
+    end
+  end
 end
