ee6cd66f0cc8bcbeff059fc0dbf798058df57a62
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-02-03 20:35:10 +0900

    commentsコントローラーにdestroyアクション作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,16 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @post = @comment.post
+    if @comment.destroy
+      respond_to :js
+    else
+      flash[:alert] = "コメントの削除に失敗しました"
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:user_id, :post_id, :comment)
